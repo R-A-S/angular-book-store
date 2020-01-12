7be81335c7d727e2594fd3cbc6e2a8171358fe04
R-A-S <r.a.s@live.ru>
2020-01-12 21:11:10 +0200

    count input alive

--- src/app/books-card-page/books-card-page.component.ts
+++ src/app/books-card-page/books-card-page.component.ts
@@ -13,11 +13,43 @@ import { Book } from '../shared/interfaces';
 export class BooksCardPageComponent implements OnInit {
   book$: Observable<Book>;
 
+  count = 1;
+
   constructor(private route: ActivatedRoute, private api: ApiService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.book$ = this.route.params.pipe(
       switchMap((params) => this.api.fetchBook(params.id)),
     );
   }
+
+  decreaseCount(): void {
+    this.count -= 1;
+  }
+
+  increaseCount(): void {
+    this.count += 1;
+  }
+
+  // validateCount(operation) {
+  //   // const {
+  //   //   cart,
+  //   //   book: { id, count: bookCount },
+  //   // } = this.props;
+  //
+  //   // const cartItem = cart.find((book) => book.id === id);
+  //   // const freeCount = cartItem
+  //   //   ? this.book$.count - cartItem.currentCount
+  //   //   : this.book$.count;
+  //   const freeCount = 5;
+  //   if (operation !== undefined) {
+  //     const isValid = operation ? this.count < freeCount : this.count > 0;
+  //     if (!isValid) {
+  //       return freeCount > 0 ? this.count : 0;
+  //     }
+  //     return operation ? this.count + 1 : this.count - 1;
+  //   }
+  //
+  //   return freeCount === 0 ? 0 : 1;
+  // }
 }
