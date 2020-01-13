30b96c7a42601a2ba433162f57b84bd297560dba
R-A-S <r.a.s@live.ru>
2020-01-13 11:51:27 +0200

    delete book  service

--- src/app/shared/book.resolver.ts
+++ src/app/shared/book.resolver.ts
@@ -5,13 +5,10 @@ import {
 } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { BookService } from './services/book.service';
 import { Book } from './interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class BookResolver implements Resolve<Book> {
-  constructor(private bookService: BookService) {}
-
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
